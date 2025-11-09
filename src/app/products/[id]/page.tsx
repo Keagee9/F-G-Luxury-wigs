5f37a86f3b3c9dd18560390409d0a339812b9df9
Kingsley Francis <kingsleyfrancis.kalu@gmail.com>
2025-11-09 07:42:09 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import { Star, Truck, ShieldCheck, Plus, Minus } from 'lucide-react';
 import { Separator } from '@/components/ui/separator';
-import { PlaceHolderImages } from '@/lib/placeholder-images';
+import PlaceHolderImages from '@/lib/placeholder-images.json';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   Accordion,

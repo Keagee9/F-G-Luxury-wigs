33f9ee4d82f85e604ee8d64420f39ff8778cc6d2
Kingsley Francis <kingsleyfrancis.kalu@gmail.com>
2025-11-09 09:38:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9901116827-b1bb0",
+  "appId": "1:250595629827:web:6f75cb6962daadab006736",
+  "apiKey": "AIzaSyAyS5wfjXbU2TZqk0UDCJZuFlWNO85qxlY",
+  "authDomain": "studio-9901116827-b1bb0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "250595629827"
+};
